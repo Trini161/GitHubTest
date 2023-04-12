167c9b4ed995519ce454c6845a8f4120d5234910
Chimp56 <47754918+Chimp56@users.noreply.github.com>
2023-04-11 22:27:20 -0500

    fixed

--- create.py
+++ create.py
@@ -1,49 +1,47 @@
-#IDEA
-# make a list of groceries
-# The user will type in an item in a list of groceries.
-#  The output will be a picture of the item
-# If the item is not an item in the list ask again 
-# if the item is in the list make item picture appear
-# or if they say apple and there is an option of green or red ask 
-# def 
-
-
 from PIL import Image
 import matplotlib.pyplot as plt
+
+# Define the list of grocery items
+groceries = ["GreenApple", "RedApple", "WaterBottle"]
+
+def get_input():
+    # Display the list of available grocery items to the user
+    print("Available grocery items:")
+    for item in groceries:
+        print("- " + item)
+    input_item = input("Enter the name of the grocery item: ")
+    if ("apple" in input_item.lower()):
+        # If the user entered "apple", prompt for the color
+        input_item = input("Enter the color of the apple: ")
+        input_item = input_item.capitalize() + "Apple"
+    else:
+        input_item = input_item.capitalize()
+    if input_item in groceries:
+        return input_item
+    else:
+        print("Invalid item.")
+        return get_input()
+
 def main():
-    # Ask for input from the user
-    groceries = ["GreenApple", "RedApple", "WaterBottle"]
-    def get_input():
-        input_item = input("Enter the name of the grocery item from groceries ")
-        if ("apple" in input_item.lower()):
-            input_item = input("Enter the color of the apple ")
-            if input_item.lower() in groceries:
-                return input_item
-        if input_item.lower() in groceries:
-            
-            return input_item
-        else:
-            print("Please enter a valid item")
-            return get_input()
-        
+    # Get input from the user
     input_item = get_input()
 
-    # Check if the user input is in the list of groceries
-    if input_item.lower() in groceries:
-        # Get the path of the image file
-        image_path = f"{input_item}.jpg"
-    # Construct the image file path based on user input
+    # Construct the path to the image file
+    image_path = f"{input_item}.jpg"
 
     try:
-        # Open the image file
+        # Open and display the image file
         img = Image.open(image_path)
-
-        # Display the image
         plt.imshow(img)
         plt.axis('off')
         plt.show()
     except FileNotFoundError:
         print("Image not found.")
+
+    # Allow the user to select another item
+    choice = input("Do you want to select another item? (y/n) ")
+    if choice.lower() == "y":
+        main()
 
 if __name__ == "__main__":
     main()
