c78870bcc385ec5be2c042aeaaa19877f2ab6033
Trini161 <97135459+Trini161@users.noreply.github.com>
2023-04-11 21:21:51 -0500

    Add files via upload

--- multifactorgui.py
+++ multifactorgui.py
@@ -1,11 +1,13 @@
 # Module multifactorgui.py
+# Includes new methods for activity 2.1.5
+
 import tkinter as tk
 import tkinter.messagebox as mb
 
 # MultiFactorAuth is a class with three frames: 
 #    an authorization (username/password) frame
 #    an authentication (information factor) frame
-#    the restircted application frame
+#    the restircted applicaiton frame
 # Users must pass all authorization and authentication steps to access the restricted app
 
 class MultiFactorAuth(tk.Tk):
@@ -84,12 +86,12 @@ class MultiFactorAuth(tk.Tk):
   def authenticate(self):
     factor_info = self.ent_auth.get()
     if(factor_info == self.answer):
-        # authenticated - create the restricted app 
+        # authenticted - create the restricted app 
         self.frame_restrict = tk.Frame(self, bg="sienna2")
         self.title("Welcome to the Restricted Application")
         self.frame_restrict.grid(row=0, column=0, sticky="news")
   
-        lbl_msg = tk.Label(self.frame_restrict,text="This is a restricted application.", bg="sienna2")
+        lbl_msg = tk.Label(self.frame_restrict,text="This is a restricted appliation.", bg="sienna2")
         lbl_msg.config(font=("Arial", 18))
         lbl_msg.pack(pady=15)
 
@@ -101,3 +103,60 @@ class MultiFactorAuth(tk.Tk):
     else:
         mb.showinfo("Authentication","We're sorry, but our records do not match your entry.")
 
+ # Reset 
+  def reset_authorizaiton(self):
+    student_username = self.ent_username.get()
+    student_password = self.ent_password.get()
+    if (student_username == self.username and  student_password == self.password):
+        # reset
+        self.frame_reset = tk.Frame(self)
+        self.title("RESET ")
+        self.frame_reset.grid(row=0, column=0, sticky='news')
+
+        self.lbl_reset_username = tk.Label(self.frame_reset,text='RESET Username:')
+        self.lbl_reset_username.pack(pady=5)
+        self.ent_reset_username = tk.Entry(self.frame_reset, bd=3)
+        self.ent_reset_username.pack(pady=5)
+
+        self.lbl_reset_passwd = tk.Label(self.frame_reset,text='RESET Password: ')
+        self.lbl_reset_passwd.pack(pady=5)
+        self.ent_reset_password = tk.Entry(self.frame_reset, show="*", bd=3)
+        self.ent_reset_password.pack(pady=5)
+
+        self.btn_reset_auth = tk.Button(self.frame_reset, text='SET new Username and Password', command=self.set_authorization_UI)
+        self.btn_reset_auth.pack(pady=15)
+        
+        # show this   frame
+        self.frame_reset.tkraise()
+
+    else:
+        mb.showinfo('Login failed','Invalid username and/or password')
+   
+     # get current password
+  def get_password(self):
+    return self.password
+
+  def get_authorization(self):
+    s1 = [None]*20
+    a = self.username
+    b = self.password
+
+    i = 0
+    for c in a:
+      s1[i] = c
+      i += 1   
+    s1[10] = ":"
+    i = 11
+    for c in b:
+      s1[i] = c
+      i += 1
+
+    s2 = "["
+    for c in s1:
+      if c == None:
+        s2 = s2 + " "
+      else:
+        s2 = s2 + c
+    s2 = s2 + "]"
+    
+    return s2
